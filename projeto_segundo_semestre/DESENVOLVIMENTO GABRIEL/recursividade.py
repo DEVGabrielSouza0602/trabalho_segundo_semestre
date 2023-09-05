fa678c5d6aca5d986ab3476fbc66ba77dda08939
DEVGabrielSouza0602 <gabrielsouza060202@hotmail.com>
2023-09-05 16:43:31 -0300

    att

--- projeto_segundo_semestre/DESENVOLVIMENTO GABRIEL/recursividade.py
+++ projeto_segundo_semestre/DESENVOLVIMENTO GABRIEL/recursividade.py
@@ -10,6 +10,8 @@ nome_dos_alimentos = dicionario_de_alimentos.keys()
 calorias_dos_alimentos = list(dicionario_de_alimentos.values())
 tamanho_da_lista_dos_alimentos = len(nome_dos_alimentos)
 contagem_de_alimentos = 0
+loop_contagem_de_calorias = True
+Alimento_valido = None
 
 
 def recursividade_calorias(contagem_de_calorias, lista):
@@ -22,18 +24,29 @@ def apresentacao_dos_alimentos(chaves_de_alimentos):
         print(f'[{posicao}] = {valor}')
 
 
-def contador_de_alimentos_funcao(chaves_de_alimentos, contador_de_alimentos):
+def contador_de_alimentos_funcao(chaves_de_alimentos):
+    contador_de_alimentos = -1
     for i in chaves_de_alimentos:
         contador_de_alimentos += 1
+    return contador_de_alimentos
 
 
 contagem_de_alimentos = contador_de_alimentos_funcao(
-    nome_dos_alimentos, contagem_de_alimentos)
+    nome_dos_alimentos)
 
 apresentacao_dos_alimentos(nome_dos_alimentos)
+while loop_contagem_de_calorias:
 
-alimento = int(input("\nDigite a posição do seu alimento: "))
-if alimento <= contagem_de_alimentos:
-    print(calorias_dos_alimentos[alimento])
-else:
-    print('O alimento não esta incluso a sua dieta.')
+    alimento = input("\nDigite a posição do seu alimento: ")
+    try:
+        alimento = int(alimento)
+        if alimento >= 0:
+            Alimento_valido = True
+    except:
+        print('Digite um alimento valido.')
+
+    if Alimento_valido is True:
+        if alimento <= contagem_de_alimentos:
+            print(calorias_dos_alimentos[alimento])
+        elif alimento > contagem_de_alimentos:
+            print('Alimento não esta incluso na dieta')
