c5cc862dfdf487b784d5b6a26cd3740d79e3f33f
Emanuelprime <emanuelll3000@gmail.com>
2023-09-05 17:15:33 -0300

    add funcao

--- projeto_segundo_semestre/desenvolvimento Emanuel/Tentativa de agua a tomar.py
+++ projeto_segundo_semestre/desenvolvimento Emanuel/Tentativa de agua a tomar.py
@@ -1,5 +1,39 @@
 '''Quant de agua a tomar'''
-Peso = float(input("Insira seu peso em Kg: "))
-# 35 é a quantidade de Ml que precisa por peso (isso é uma média)
-quant_agua = Peso * 35
-print(f'Você precisa tomar cerca de {quant_agua: .2f}Ml de água por dia')
+
+
+def calcular_quantidade_agua(peso, formula="padrao"):
+    if formula == "padrao":
+        return peso * 30
+    elif formula == "atleta":
+        return peso * 40
+    elif formula == "personalizado":
+        personalizacao = float(
+            input("Informe a quantidade personalizada de água (ml/kg): "))
+        return peso * personalizacao
+    else:
+        return ("Fórmula de cálculo não reconhecida")
+
+
+peso = float(input("Informe o seu peso em Kg: "))
+
+# escolher uma fórmula de cálculo
+print("Escolha a fórmula mais adequada para o cálculo:")
+print("1 - Fórmula padrão (30 ml/kg)")
+print("2 - Fórmula para atletas (40 ml/kg)")
+print("3 - Fórmula personalizada")
+formula_escolhida = int(input("Digite o número da opção desejada: "))
+
+# calcula a quantidade recomendada de água com base na escolha da fórmula
+if formula_escolhida == 1:
+    quantidade_agua_ml = calcular_quantidade_agua(peso, "padrao")
+elif formula_escolhida == 2:
+    quantidade_agua_ml = calcular_quantidade_agua(peso, "atleta")
+elif formula_escolhida == 3:
+    quantidade_agua_ml = calcular_quantidade_agua(peso, "personalizado")
+else:
+    print("Opção inválida.")
+    quantidade_agua_ml = 0
+
+if quantidade_agua_ml > 0:
+    print(
+        f"Você deve tomar aproximadamente {quantidade_agua_ml:.2f} ml de água por dia.")
