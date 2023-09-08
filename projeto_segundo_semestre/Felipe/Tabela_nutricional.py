1e29a9603df6cba7a85be6cdbf2550c9e837fb89
LipeWendler <felipewendlerccbeu@gmail.com>
2023-09-07 21:47:21 -0300

    att

--- projeto_segundo_semestre/Felipe/Tabela_nutricional.py
+++ projeto_segundo_semestre/Felipe/Tabela_nutricional.py
@@ -12,12 +12,12 @@ def size(lista):
     return len(lista)
 
 
-alimento = ['a', 'b', 'c', 'd', 'e',]
-quantidade_teste = [1, 2, 3, 4, 5]
-valor_energético = [10, 20, 30, 40 ,50]
-carboidratos = [5, 10, 15, 20, 25]
-proteinas = [3, 6, 9, 12, 15]
-sodio = [10, 20, 30 ,40 ,50]
+alimento = ['Frango', 'Arroz branco', 'Feijão preto', 'Batata doce', 'Banana',]
+quantidade_teste = [100, 100, 100, 100, 100]
+valor_energético = [139, 130, 76, 100 ,89]
+carboidratos = [0.3, 28.2, 14, 24, 23]
+proteinas = [26, 2.7, 4.3, 2, 1.1]
+sodio = [153, 1, 99 , 36, 1]
 
 permanecer_tabela = True
 
@@ -50,12 +50,12 @@ while permanecer_tabela:
             print(70*"=")
             print("                TABELA NUTRICIONAL")
             print(50*"-")
-            print(f"Em {quantidade_teste[escolha_alimento]}g de", alimento[escolha_alimento], "existe:")
+            print(f"Em {quantidade_teste[escolha_alimento]}g de {alimento[escolha_alimento]}:")
             print(50*"-")
             print(f"Valor energético: {valor_energético[escolha_alimento]}kcal")
             print(f"Carboidratos: {carboidratos[escolha_alimento]}g") 
             print(f"Proteinas: {proteinas[escolha_alimento]}g")
-            print(f"Sódio: {sodio[escolha_alimento]}g")
+            print(f"Sódio: {sodio[escolha_alimento]}mg")
             print(50*"=")
 
             time.sleep(1)
@@ -86,15 +86,34 @@ while permanecer_tabela:
         print(50*"-")
         sodio_novo = float(input("Digite o quantidade de sódio (em gramas): "))
         time.sleep(1)
-
-        push(alimento, novo_alimeto)
-        push(quantidade_teste, quantidade_nova)
-        push(valor_energético, valor_novo)
-        push(carboidratos, carboidratos_novo)
-        push(proteinas, proteina_nova)
-        push(sodio, sodio_novo)
-        
         print(50*"=")
-        voltar = input("Deseja permanecer em Tabela Nutricional? Sim ou Não? ").lower().startswith('n')
-        if voltar == True:
-            permanecer_tabela = False
+
+        os.system("cls")
+        time.sleep(1)
+        print(70*"=")
+        print("                TABELA NUTRICIONAL")
+        print(50*"-")
+        print(f"Em {quantidade_nova}g de {novo_alimeto}:")
+        print(50*"-")
+        print(f"Valor energético: {valor_novo}kcal")
+        print(f"Carboidratos: {carboidratos_novo}g") 
+        print(f"Proteinas: {proteina_nova}g")
+        print(f"Sódio: {sodio_novo}mg")
+        print(50*"=")
+
+        adicionar = input(f"Confirmar a adição: Sim ou Não? ").lower().startswith('s')
+        if adicionar == True:
+            push(alimento, novo_alimeto)
+            push(quantidade_teste, quantidade_nova)
+            push(valor_energético, valor_novo)
+            push(carboidratos, carboidratos_novo)
+            push(proteinas, proteina_nova)
+            push(sodio, sodio_novo)
+        
+        else:
+            os.system("cls")
+            time.sleep(1)
+            print(50*"=")
+            voltar = input("Deseja permanecer em Tabela Nutricional? Sim ou Não? ").lower().startswith('n')
+            if voltar == True:
+                permanecer_tabela = False
