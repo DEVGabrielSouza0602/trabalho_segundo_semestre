0f216e0f905d74800d9841a0d40875e3c89c0336
LipeWendler <felipewendlerccbeu@gmail.com>
2023-09-10 21:38:49 -0300

    att

--- projeto_segundo_semestre/Felipe/Dietas.py
+++ projeto_segundo_semestre/Felipe/Dietas.py
@@ -13,15 +13,24 @@ print("[3] Moderado (3 a 5 dias na semana)")
 print("[4] Pesado (6 a 7 dias na semana)")
 print("[5] Muito pesado (diariamente ou 2X ao dia)")
 print(70*"-")
-escolha = int(input("Digite o indice de acordo com a frequência que você se exercita: "))
+escolha_frequencia = int(input("Digite o indice de acordo com a frequência que você se exercita: "))
 
-if escolha == 1:
+if escolha_frequencia == 1:
     frequencia = 1.2
-elif escolha == 2:
+elif escolha_frequencia == 2:
     frequencia = 1.375
-elif escolha == 3:
+elif escolha_frequencia == 3:
     frequencia = 1.55
-elif escolha == 4:
+elif escolha_frequencia == 4:
     frequencia = 1.725
-elif escolha == 5:
+elif escolha_frequencia == 5:
     frequencia = 1.9
+
+os.system("cls")
+
+print(70*"=") #Cabeçario das Dietas
+print("                          DIETAS BÁSICAS")
+print(70*"=")
+print("[1] Emagrecer")
+print("[2] Ganhar de massa muscular")
+print(70*"-")
